f7d544ba8fae8f5997886175b19ed4a462edc1e7
evan <evan@sdicg.com>
2014-07-08 10:26:54 -0400

    attempting to add card submission task

--- tasks/card.js
+++ tasks/card.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function(grunt, exec) {
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
@@ -31,7 +31,15 @@ module.exports = function(grunt) {
 			}
 		},
 		gitcommit: {
-			release: {
+			describe: {
+				files: {
+					src: ['etc/branch_description.json']
+				},
+				options:{
+					message: 'describing the current branch'
+				}
+			}
+			, release: {
 				files: {
 					src: ['package.json', 'bower.json']
 				},
@@ -64,6 +72,8 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		gitmerge: {
+		},
 		prompt: {
 			startCard: {
 				options: {
@@ -86,27 +96,57 @@ module.exports = function(grunt) {
 		}
 	});
 
-	grunt.registerTask('card', 'start a new card',[
-			'prompt:startCard',	// ask what the user is working on
-			'gitcheckout:dev', // check out dev
-			'gitpull:dev', // pull dev
-			'startCard' // perform git operations to begin a new card
+	grunt.registerTask('card', 'start a new card', [
+		'prompt:startCard'     // ask what the user is working on
+		, 'gitcheckout:dev'    // check out dev
+		, 'gitpull:dev'        // pull dev
+		, 'card:start'         // perform git operations to begin a new card
+		, 'gitcheckout:branch' // check out the branch
+		, 'card:describe'      // create file in repo to describe the current branch
+		, 'gitcommit:describe' // commit description to new branch
 	]);
 
-	grunt.registerTask('startCard', 'perform git operations to begin a new card', function(){
-		// start and check out a new branch whose name is based on user's answer
-		var branchTitle = grunt.config('cardDescription'),
-		    branchType  = grunt.config('cardType');
+	grunt.registerTask('card:submit', 'submit what you are working on for review', [
+		'branch:note'
+		, 'gitcheckout:dev' // check out dev
+		, 'gitpull:dev' // pull dev
+		, 'branch:review' // merge in the random branch
+		, 'gitcheckout:branch'
+		, 'gitmerge'
+	]);
 
-		branchTitle = branchType+"_"+camelCaseSentence(branchTitle);
-		grunt.config.set('gitcheckout.branch.options.branch', branchTitle)
-		grunt.task.run('gitcheckout:branch');
-		grunt.log.writeln(branchTitle);
+	grunt.registerTask('card:describe', 'create file in repo to describe the current branch', function(){
+		var branchDescription = 
+		    	{ title: grunt.config('cardDescription')
+		    	, type: grunt.config('cardType')
+		    	}
+
+		grunt.file.write('./etc/branch_description.json', JSON.stringify(branchDescription , null, "\t"))
 	});
 
-	grunt.registerTask('card:submit', 'submit what you are working on for review', function() {
-		// note the current branch
-		//
+	grunt.registerTask('card:start', 'perform git operations to begin a new card', function(){
+		// start and check out a new branch whose name is based on user's answer
+		var branchTitle = grunt.config('cardDescription')
+		    , branchType  = grunt.config('cardType');
+
+		// set the branch title
+		grunt.config.set('gitcheckout.branch.options.branch', makeBranchName(branchType, branchTitle);
+	});
+
+	grunt.registerTask('branch', 'create and merge random branches to and from dev', function(merge){
+		var branch;
+		switch(merge){
+			case 'note':
+				branch = grunt.file.readJSON('./etc/branch_description.json');
+				grunt.config.set('gitmerge.branch.options.branch', makeBranchName(branch.type, branch.title);
+			case 'review':
+				grunt.config.set('gitmerge.branch.options.branch', makeBranchName('review', branch.title, branch.type));
+				break;
+			default:
+				break;
+		}
+	});
+
 		// pull
 		//
 		// check out dev
@@ -124,7 +164,6 @@ module.exports = function(grunt) {
 		//  - if no errors, push to origin
 		//
 		// if errors, tell user to correct conflicts and submit again
-	});
 
 	grunt.registerTask('card:review', 'review a branch for submission', function() {
 		// user selects branch to review from list of available branches
@@ -192,4 +231,12 @@ function camelCaseSentence(input){
 	});
 
 	return input.join("");
+}
+
+function makeBranchName(type, title, descriptor){
+	var result = type+"_"+camelCaseSentence(title);  
+	if(descriptor){
+		result += "_"+descriptor;
+	}
+	return result;
 }
